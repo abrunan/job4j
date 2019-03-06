b1c67db93899f4553569ec93fe58624d7baa4954
Hunan Abrahamyan <abrunan@yandex.ru>
2019-03-06 22:52:02 +0300

    1.3. JavaDoc. Code convention[#94694]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+	* Class Calculate
+	* This class is for arithmetic operations: + - * / ^.
+	* @author abrunan
+	* @since 06.03.2019
+	* @version 1
+*/
 public class Calculate {
 	
+	/**
+		* Method main() is the entry point to the application
+		* @param args - an array of Strings passed from the console
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
