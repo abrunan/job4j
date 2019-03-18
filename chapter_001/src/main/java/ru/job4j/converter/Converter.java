25a47a293a7a3082d77e0e9e91863f0d2c5cd1dd
Hunan Abrahamyan <abrunan@yandex.ru>
2019-03-18 21:34:20 +0300

    Finish class Converter, remove main() method from class Calculate

--- chapter_001/src/main/java/ru/job4j/converter/Converter.java
+++ chapter_001/src/main/java/ru/job4j/converter/Converter.java
@@ -12,38 +12,38 @@ public class Converter {
 
     /**
      * Convert rubles to euros
-     * @param value
+     * @param value to be converted
      * @return euros
      */
     public int rubleToEuro(int value) {
-        return -1;
+        return value / 70;
     }
 
     /**
      * Convert rubles to USD
-     * @param value
+     * @param value to be converted
      * @return dollars
      */
     public int rubleToDollar(int value) {
-        return -1;
+        return value / 60;
     }
 
     /**
      * Convert euros to rubles
-     * @param value
+     * @param value to be converted
      * @return rubles
      */
     public int euroToRuble(int value) {
-        return -1;
+        return value * 70;
     }
 
     /**
      * Convert US dollars to rubles
-     * @param value
+     * @param value to be converted
      * @return rubles
      */
     public int dollarToRuble(int value) {
-        return -1;
+        return value * 60;
     }
 
 }
