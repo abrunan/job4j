c57455ca1c17f8c5d962a9c2d38045513b3bc751
Hunan Abrahamyan <abrunan@yandex.ru>
2019-03-20 00:51:15 +0300

    3.4. Расстояние между точками в системе координат[#94686] Add class Point and test class - PointTest.

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+public class Point {
+
+    public double distance (int x1, int x2, int y1, int y2) {
+        return sqrt(
+                pow(x1 - x2, 2) +
+                pow(y1 - y2, 2)
+        );
+    }
+}
