dbdcfcd60585762e6da5dd79e423bd122f98db00
Hunan Abrahamyan <abrunan@yandex.ru>
2019-03-20 12:20:57 +0300

    4.1. Глупый бот.[#94657]\n Add class DummyBot and test class - DummyBotTest.

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+/**
+ * Class DummyBot
+ * Practicing conditional operators.
+ * @author abrunan
+ * @since 20.03.2019
+ * @version 1
+ */
+
+public class DummyBot {
+
+    /**
+     * Answers to questions
+     * @param question Your question
+     * @return An answer to your questions
+     */
+    public String answer(String question) {
+        String ans = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question))
+            ans = "Привет, умник.";
+        else if ("Пока.".equals(question))
+            ans = "До скорой встречи.";
+
+        return ans;
+    }
+}
