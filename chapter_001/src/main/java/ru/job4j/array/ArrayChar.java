7d80637acec4f3534d51a457d6cad42906215395
Hunan Abrahamyan <abrunan@yandex.ru>
2019-04-08 18:34:55 +0300

    6.4. Слово начинается с ...[#94674]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,31 @@
+package ru.job4j.array;
+
+/**
+ * Wrapper class for String
+ */
+public class ArrayChar {
+    private char[] data;
+
+    public ArrayChar(String line) {
+        this.data = line.toCharArray();
+    }
+
+    /**
+     * Check if the string starts with the prefix
+     * @param prefix - the prefix to check for
+     * @return true if the string starts with the prefix, false in the other case
+     */
+    public boolean startWith(String prefix) {
+        boolean result = true;
+        char[] pref = prefix.toCharArray();
+
+        for (int i = 0; i < pref.length; i++) {
+            if (pref[i] != this.data[i]) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class ArrayCharTest {
+
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("He");
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenStartWithPrefixThenFalse() {
+        ArrayChar word = new ArrayChar("Hello");
+        boolean result = word.startWith("Hi");
+        assertThat(result, is(false));
+    }
+}
