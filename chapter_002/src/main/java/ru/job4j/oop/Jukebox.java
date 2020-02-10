7f871a4c6a3262b6c45c64d3cf94bc338934afeb
Hunan Abrahamyan <abrunan@yandex.ru>
2020-02-10 19:59:32 +0300

    Invoking a method with parameters. 1.3. Вызов метода с аргументами. [#166086]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+    public static void main(String[] args) {
+        Jukebox juke = new Jukebox();
+        juke.music(1);
+        juke.music(42);
+        juke.music(2);
+    }
+
+    public void music(int position) {
+        if (position == 1) {
+            System.out.println("Пусть бегут неуклюже");
+        } else if (position == 2) {
+            System.out.println("Спокойной ночи");
+        } else System.out.println("Песня не найдена");
+    }
+}
