03fe50645b54d30ae75125c150e7b4b871df2e54
Hunan Abrahamyan <abrunan@yandex.ru>
2020-02-12 20:37:34 +0300

    Fields of an object 1.5. Поля объекта [#166083]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop;
+
+public class Cat {
+    private String food;
+    private String name;
+
+    public void eat (String meat) {
+        this.food = meat;
+    }
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+    public void show () {
+        System.out.println(this.name + " eats " + this.food);
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        Cat black = new Cat();
+        black.giveNick("Big black cat");
+        black.eat("fish");
+        black.show();
+    }
+}
