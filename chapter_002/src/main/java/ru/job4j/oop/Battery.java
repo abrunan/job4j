fc44bf29bce7c1a6393a9070d8163cd75e484750
Hunan Abrahamyan <abrunan@yandex.ru>
2020-02-13 16:55:42 +0300

    Object's state 1.7. Состояние объекта. [#167388]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,21 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+    private void exchange(Battery another) {
+        another.load  += this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(10);
+        Battery second = new Battery(15);
+        System.out.println(first.load + " " + second.load);
+        first.exchange(second);
+        System.out.println(first.load + " " + second.load);
+    }
+}
