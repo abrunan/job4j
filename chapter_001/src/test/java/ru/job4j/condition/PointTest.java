e56b708ce59f5a7dc2bd7ced1aeeb33783a2a640
Hunan Abrahamyan <abrunan@yandex.ru>
2020-02-14 15:23:31 +0300

    Distance between points in 3D 4. Расстояние между точками в трехмерном пространстве. [#122643]

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -14,4 +14,12 @@ public class PointTest {
         double expect = 10D;
         assertThat(result, is(expect));
     }
+    @Test
+    public void whenZeroAndTenThenTen3d() {
+        Point point0 = new Point(0,0, 0);
+        Point point1 = new Point(0,0, 10);
+        double result = point0.distance3d(point1);
+        double expect = 10D;
+        assertThat(result, is(expect));
+    }
 }
