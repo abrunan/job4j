b42c1965e50456b629dfa21e7ed8c15602e8dd55
Hunan Abrahamyan <abrunan@yandex.ru>
2020-03-06 11:13:41 +0300

    Testing static methods 5.1. Тестирование. Подготовка данных. [#182960]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -16,4 +16,31 @@ public class StartUITest {
         Item expected = new Item("Bruh, my PC is broken...");
         assertThat(created.getName(), is(expected.getName()));
     }
+
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Bruh!");
+        tracker.add(item);
+        String[] answers = {
+                item.getId(),
+                "Bruh, you've been replaced!"
+        };
+        Input input = new StubInput(answers);
+        StartUI.replaceItem(input, tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("Bruh, you've been replaced!"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Bruh!");
+        tracker.add(item);
+        String[] answer = { item.getId() };
+        Input input = new StubInput(answer);
+        StartUI.deleteItem(input, tracker);
+        String expected = null;
+        assertThat(tracker.findById(item.getId()), is(expected));
+    }
 }
