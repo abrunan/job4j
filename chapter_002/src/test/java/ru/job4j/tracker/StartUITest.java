3ff6857d9125f0d382b80c70ddcb0e762ac4d1e0
Hunan Abrahamyan <abrunan@yandex.ru>
2020-03-14 18:24:43 +0300

    Tests for StartUI 9. Написать тесты на StartUI. [#785]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -8,11 +8,22 @@ import static org.junit.Assert.assertThat;
 public class StartUITest {
 
     @Test
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+                );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] { action });
+        assertThat(action.isCall(), is(true));
+    }
+
+    @Test
     public void whenCreateItem() {
         String[] answers = {"Bruh, my PC is broken..."};
         Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
+        UserAction create = new CreateAction();
+        create.execute(input, tracker);
         Item created = tracker.findAll()[0];
         Item expected = new Item("Bruh, my PC is broken...");
         assertThat(created.getName(), is(expected.getName()));
@@ -28,7 +39,8 @@ public class StartUITest {
                 "Bruh, you've been replaced!"
         };
         Input input = new StubInput(answers);
-        StartUI.replaceItem(input, tracker);
+        UserAction replace = new ReplaceAction();
+        replace.execute(input, tracker);
         Item replaced = tracker.findById(item.getId());
         assertThat(replaced.getName(), is("Bruh, you've been replaced!"));
     }
@@ -40,7 +52,8 @@ public class StartUITest {
         tracker.add(item);
         String[] answer = {item.getId()};
         Input input = new StubInput(answer);
-        StartUI.deleteItem(input, tracker);
+        UserAction delete = new DeleteAction();
+        delete.execute(input, tracker);
         String expected = null;
         assertThat(tracker.findById(item.getId()), is(expected));
     }
