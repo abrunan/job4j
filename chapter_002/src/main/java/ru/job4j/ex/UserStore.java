6a2a6341d6f62846b4c4f157114849f0108cc848
Hunan Abrahamyan <abrunan@yandex.ru>
2020-03-26 00:10:46 +0300

    Exception hierarchy 0.5. Иерархия исключений и множественный catch. [#219368]

---
+++ chapter_002/src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,39 @@
+package ru.job4j.ex;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User res = null;
+        for (int i = 0; i < users.length; i++) {
+            if (users[i].getUsername().equals(login)) {
+                res = users[i];
+            }
+        }
+        if (res == null) {
+            throw new UserNotFoundException("User is not found");
+        }
+        return res;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("User is not valid");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("abrunan", true)
+        };
+        try {
+            User user = findUser(users, "abrunan");
+            if (validate(user)) {
+                System.out.println("This user has access");
+            }
+        } catch (UserInvalidException e) {
+            e.printStackTrace();
+        } catch (UserNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
