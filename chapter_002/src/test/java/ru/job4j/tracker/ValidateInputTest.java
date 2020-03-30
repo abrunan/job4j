1422ab8a1a755922fa42f23d3629d485768ec7ed
Hunan Abrahamyan <abrunan@yandex.ru>
2020-03-30 21:57:41 +0300

    ValidateInput tests 1.1. Тесты на ValidateInput. [#223313][#261274]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.tracker;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class ValidateInputTest {
+    private final PrintStream sdtOut = System.out;
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("Execute before test");
+        System.setOut(new PrintStream(out));
+    }
+
+    @After
+    public void backOutput() {
+        System.out.println("Execute after test");
+        System.setOut(sdtOut);
+    }
+
+    @Test
+    public void whenInvalidInput() {
+        String[] data = {"one"};
+        ValidateStubInput input = new ValidateStubInput(data);
+        input.askInt("Enter number");
+        assertThat(
+                out.toString(),
+                is(String.format("It is not a number. Please enter again.%n"))
+        );
+    }
+
+    @Test
+    public void whenOutOfBounds() {
+        String[] data = {"7"};
+        ValidateStubInput input = new ValidateStubInput(data);
+        input.askInt("Enter number", 6);
+        assertThat(
+                out.toString(),
+                is(String.format("The number is out of bounds. Please enter again.%n"))
+        );
+    }
+}
