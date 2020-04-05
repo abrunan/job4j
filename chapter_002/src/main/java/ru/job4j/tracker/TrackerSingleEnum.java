4982ce52d8eb9f015158073edf98c2e985b89e47
Hunan Abrahamyan <abrunan@yandex.ru>
2020-04-05 16:23:13 +0300

    Tracker Singleton tests

--- chapter_002/src/main/java/ru/job4j/tracker/TrackerSingleEnum.java
+++ chapter_002/src/main/java/ru/job4j/tracker/TrackerSingleEnum.java
@@ -2,4 +2,9 @@ package ru.job4j.tracker;
 
 public enum TrackerSingleEnum {
     INSTANCE;
+    private Tracker tracker = new Tracker();
+
+    public Tracker getTracker() {
+        return tracker;
+    }
 }
---
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerSinglesTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertSame;
+
+public class TrackerSinglesTest {
+
+    @Test
+    public void whenEnum() {
+        TrackerSingleEnum tse = TrackerSingleEnum.INSTANCE;
+        Tracker tracker0 = tse.getTracker();
+        Tracker tracker1 = tse.getTracker();
+        assertSame(tracker0, tracker1);
+    }
+
+    @Test
+    public void whenInnerCl() {
+        Tracker tracker0 = TrackerSingleInnerCl.getInstance();
+        Tracker tracker1 = TrackerSingleInnerCl.getInstance();
+        assertSame(tracker0, tracker1);
+    }
+
+    @Test
+    public void whenInnerSF() {
+        Tracker tracker0 = TrackerSingleInnerCl.getInstance();
+        Tracker tracker1 = TrackerSingleInnerCl.getInstance();
+        assertSame(tracker0, tracker1);
+    }
+
+    @Test
+    public void whenInnerSFF() {
+        Tracker tracker0 = TrackerSingleInnerCl.getInstance();
+        Tracker tracker1 = TrackerSingleInnerCl.getInstance();
+        assertSame(tracker0, tracker1);
+    }
+
+}
