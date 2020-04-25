099b6ab873e71f99021b04496f5c6c4bcde01f49
Hunan Abrahamyan <abrunan@yandex.ru>
2020-04-25 20:34:33 +0300

    Bank transfer, BankService and tests 3. Банковские переводы. [#10038] [#279147]

--- chapter_003/src/main/java/ru/job4j/bank/BankService.java
+++ chapter_003/src/main/java/ru/job4j/bank/BankService.java
@@ -1,4 +1,51 @@
 package ru.job4j.bank;
 
+import java.util.*;
+
 public class BankService {
+	private Map<User, List<Account>> users = new HashMap<>();
+
+	public void addUser(User user) {
+		users.putIfAbsent(user, new ArrayList<Account>());
+	}
+
+	public void addAccount(String passport, Account account) {
+		User user = findByPassport(passport);
+		List<Account> accs = users.get(user);
+		if (!accs.contains(account)) {
+			users.get(user).add(account);
+		}
+	}
+
+	public User findByPassport(String passport) {
+		for (User user : users.keySet()) {
+			if (user.getPassport().equals(passport))
+				return user;
+		}
+		return null;
+	}
+
+	public Account findByRequisite(String passport, String requisite) {
+		User user = findByPassport(passport);
+		if (user == null) {
+			return null;
+		}
+		int index = users.get(user).indexOf(new Account(requisite, 0D));
+		return users.get(user).get(index);
+	}
+
+	public boolean transferMoney(String srcPassport, String srcRequisite,
+	                             String destPassport, String destRequisite, double amount) {
+		boolean res = false;
+		Account srcAcc = findByRequisite(srcPassport, srcRequisite);
+		Account destAcc = findByRequisite(destPassport, destRequisite);
+		if (srcAcc != null
+				&& destAcc != null
+				&& srcAcc.getBalance() >= amount) {
+			srcAcc.setBalance(srcAcc.getBalance() - amount);
+			destAcc.setBalance(destAcc.getBalance() + amount);
+			res = true;
+		}
+		return res;
+	}
 }
--- chapter_003/src/test/java/ru/job4j/bank/BankServiceTest.java
+++ chapter_003/src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -1,4 +1,46 @@
 package ru.job4j.bank;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
 public class BankServiceTest {
+	@Test
+	public void addUser() {
+		User user = new User("4606", "Bill Cipher");
+		BankService bank = new BankService();
+		bank.addUser(user);
+		assertThat(bank.findByPassport("4606"), is(user));
+	}
+
+	@Test
+	public void whenEnterInvalidPassport() {
+		User user = new User("4606", "Bill Cipher");
+		BankService bank = new BankService();
+		bank.addUser(user);
+		bank.addAccount(user.getPassport(), new Account("5546", 150D));
+		assertNull(bank.findByRequisite("34", "5546"));
+	}
+
+	@Test
+	public void addAccount() {
+		User user = new User("4606", "Bill Cipher");
+		BankService bank = new BankService();
+		bank.addUser(user);
+		bank.addAccount(user.getPassport(), new Account("5546", 150D));
+		assertThat(bank.findByRequisite("4606", "5546").getBalance(), is(150D));
+	}
+
+	@Test
+	public void transferMoney() {
+		User user = new User("4606", "Bill Cipher");
+		BankService bank = new BankService();
+		bank.addUser(user);
+		bank.addAccount(user.getPassport(), new Account("5546", 150D));
+		bank.addAccount(user.getPassport(), new Account("113", 50D));
+		bank.transferMoney(user.getPassport(), "5546", user.getPassport(), "113", 150D);
+		assertThat(bank.findByRequisite(user.getPassport(), "113").getBalance(), is(200D));
+	}
 }
