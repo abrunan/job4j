5d3893003b32078cde7a862b24b9529167866b0f
Hunan Abrahamyan <abrunan@yandex.ru>
2020-05-17 19:51:20 +0300

    Sort by number 4. Сортировка номера [#221284] [#284185]

--- chapter_003/src/main/java/ru/job4j/collections/LexSort.java
+++ chapter_003/src/main/java/ru/job4j/collections/LexSort.java
@@ -5,8 +5,8 @@ import java.util.Comparator;
 public class LexSort implements Comparator<String> {
 	@Override
 	public int compare(String left, String right) {
-		int leftNumber = Integer.parseInt(left, 0, left.indexOf('.'),10);
-		int rightNumber = Integer.parseInt(right, 0, right.indexOf('.'),10);
+		int leftNumber = Integer.parseInt(left.split("\\.")[0]);
+		int rightNumber = Integer.parseInt(right.split("\\.")[0]);
 
 		return Integer.compare(leftNumber, rightNumber);
 	}
